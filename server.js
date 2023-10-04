065afa8d6a11b5357ab2909a158eb90d61184417
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-10-04 20:25:41 +0300

    server update

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from 'express';
+import startServer from './libs/boot';
+import injectMiddlewares from './libs/middlewares';
 import injectRoutes from './routes';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 injectRoutes(app);
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
-});
+injectMiddlewares(app);
+startServer(app, port);
 
 export default app;
