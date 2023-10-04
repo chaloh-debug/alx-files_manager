ffd2d82cb00e9fcb73432dda04b93be0efebb294
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-10-04 20:15:38 +0300

    files update

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const express = require('express');
+import express from 'express';
+import injectRoutes from './routes';
 
 const app = express();
+const port = process.env.PORT || 5000;
 
+injectRoutes(app);
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
 
+export default app;
