a6043f7bc9dd9d9380bc60e284a1de2411961ed1
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-10-05 04:31:55 +0300

    update

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,22 +1,22 @@
-import { redisClient } from "../utils/redis";
-import dbClient from "../utils/db";
+import { redisClient } from '../utils/redis';
+import dbClient from '../utils/db';
 
 export default class AppController {
-    static getStatus(req, res) {
-        const status = {
-            redis: redisClient.isAlive(),
-            db: dbClient.isAlive(),
-        };
-        return res.status(200).send(status);
-    }
+  static getStatus(req, res) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    return res.status(200).send(status);
+  }
 
-    static async getStats(req, res) {
-        const users = await dbClient.nbUsers();
-        const files = await dbClient.nbFiles();
-        const status = {
-            users,
-            files,
-        };
-        return res.status(200).send(status);
-    }
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    const status = {
+      users,
+      files,
+    };
+    return res.status(200).send(status);
+  }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-import { Express } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
@@ -7,23 +6,22 @@ import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 import { APIError, errorResponse } from '../middlewares/error';
 
 const injectRoutes = (api) => {
-    api.get('/status', AppController.getStatus);
-    api.get('/stats', AppController.getStats);
-    api.post('/users', UsersController.postNew);
-    api.get('/connect', basicAuthenticate, AuthController.getConnect);
-    api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);    api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
-    api.post('/files', xTokenAuthenticate, FilesController.postUpload);
-    api.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
-    api.get('/files', xTokenAuthenticate, FilesController.getIndex);
-    api.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
-    api.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
-    api.get('/files/:id/data', FilesController.getFile);
-  
-    api.all('*', (req, res, next) => {
-      errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
-    });
-    api.use(errorResponse);
+  api.get('/status', AppController.getStatus);
+  api.get('/stats', AppController.getStats);
+  api.post('/users', UsersController.postNew);
+  api.get('/connect', basicAuthenticate, AuthController.getConnect);
+  api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect); api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
+  api.post('/files', xTokenAuthenticate, FilesController.postUpload);
+  api.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
+  api.get('/files', xTokenAuthenticate, FilesController.getIndex);
+  api.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
+  api.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
+  api.get('/files/:id/data', FilesController.getFile);
 
+  api.all('*', (req, res, next) => {
+    errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
+  });
+  api.use(errorResponse);
 };
 
 export default injectRoutes;
